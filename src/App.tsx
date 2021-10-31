55a5dfd4173b461878ca4ddaf0e99bc6c8fc1dee
Isai Lopez Rodas <40477553+ilopezro@users.noreply.github.com>
2021-10-31 15:33:04 -0700

    [179814607] Add i18n support (#8)

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import logo from "./logo.svg"
 import "./App.scss"
+import "i18n/config"
 import { FC } from "react"
+import { useTranslation } from "react-i18next"
 
 const App: FC = () => {
+  const { t } = useTranslation(["pages"])
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        Hello World to this Site
+        {t("pages:index:greeting")}
       </header>
     </div>
   )
---
+++ src/i18n/en/components.ts
@@ -0,0 +1,3 @@
+export const componentsTranslations = {}
+
+export default componentsTranslations
