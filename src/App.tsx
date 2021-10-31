680faea731384f18193eb6d685fc1f7a02e0526d
Isai Lopez Rodas <40477553+ilopezro@users.noreply.github.com>
2021-10-30 23:01:54 -0700

    [179814581] Setup JS & TS Linting  (#4)
    Set up eslint and prettier in the codebase along with their script in the package.json file.

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg"
+import "./App.css"
 
 function App() {
   return (
@@ -9,7 +9,7 @@ function App() {
         Hello World to this Site
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
