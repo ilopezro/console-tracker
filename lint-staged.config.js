6544516e63472dcd39bbe6e46e7bceef540293b1
Isai Lopez <ilopezro@ucsc.edu>
2021-10-31 18:51:21 -0700

    finalized linting for scss

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 const micromatch = require("micromatch")
 
 module.exports = {
@@ -5,5 +6,10 @@ module.exports = {
     const match = micromatch.not(files)
 
     return match.map((file) => `yarn run eslint --fix ${file}`)
+  },
+  "**/*.scss": (files) => {
+    const match = micromatch.not(files)
+
+    return match.map((file) => `yarn stylelint --aei ${file}`)
   },
 }
